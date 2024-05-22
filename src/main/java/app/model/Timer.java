371bb42b29c0f8d8bf8c770fac11aca72714048f
Drpakman <DrAlexpak@gmail.com>
2024-05-22 20:32:28 +0900

    Spring_Task#2

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,11 +1,8 @@
 package app.model;
 
-import org.springframework.stereotype.Component;
-
-
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+    private final Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
